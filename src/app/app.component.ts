6258736042da2ee8edce73a0f0eab34684a5eb92
Ignacio Buioli <ibuioli@gmail.com>
2019-02-22 10:57:29 -0300

    feat: initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'odoo-app';
+export class AppComponent implements OnInit {
+  @ViewChild('odoo_url') odoo_url: ElementRef;
+  @ViewChild('odoo_db') odoo_db: ElementRef;
+  @ViewChild('odoo_user') odoo_user: ElementRef;
+  @ViewChild('odoo_pass') odoo_pass: ElementRef;
+  public url: SafeUrl;
+  public odoo_login = true;
+  public odoo_connect = false;
+  public is_loader = false;
+
+  public alert = '';
+
+  constructor(private sanitizer: DomSanitizer) {}
+
+  public ngOnInit(): void {}
+
+  public logUrl(): void {
+    const server_url = this.odoo_url.nativeElement.value;
+    const db = this.odoo_db.nativeElement.value;
+    const user = this.odoo_user.nativeElement.value;
+    const pass = this.odoo_pass.nativeElement.value;
+
+    const regex = new RegExp('(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\
+    .[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]\
+    .[^\s]{2,}|www\.[a-zA-Z0-9]\.[^\s]{2,})');
+
+    if (server_url !== '') {
+      if (server_url.match(regex)) {
+        if (db !== '') {
+          this.url = this.sanitizer.bypassSecurityTrustResourceUrl(server_url + '/web/app?db=' + db + '&login=' + user +
+          '&password=' + pass);
+        } else {
+          this.url = this.sanitizer.bypassSecurityTrustResourceUrl(server_url + '/web/app?login=' + user +
+          '&password=' + pass);
+        }
+        this.odoo_login = false;
+        this.is_loader = true;
+        this.odoo_connect = true;
+      } else {
+        this.alert = 'You have to insert a valid URL';
+      }
+    } else {
+      this.alert = 'You have to insert an URL';
+    }
+  }
+
+  public odooLogComplete(): void {
+    this.is_loader = false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
